d67bc192b3942b73e66d742fa1618c380a13bb27
Krishna Kumar <kumarkrishna190@gmail.com>
2019-09-27 23:19:43 +0530

    A todo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      items: []
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({ term: event.target.value });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    });
+  }
+
+  render() {
+    return (
+      <div class="mw4 mw7-ns center bg-light-gray pa2 ph5-ns">
+      <h1 class="mt10 tc light-pink f-headline lh-solid">todos</h1>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input class="ma0 ph7 pa3 br2" value={this.state.term} onChange={this.onChange} placeholder="What need to be done?" />
+        </form>
+        <List class="f1" items={this.state.items} />
+      </div>
+    );
+  }
 }
-
 export default App;
